858b1f58d537b75be007f32b6f6dbb80e48eff30
Hendrik Siemund <hendrik.smd@proton.me>
2025-12-17 21:00:07 +0100

    refactor

--- src/routing/raptor/para/route_partition.cc
+++ src/routing/raptor/para/route_partition.cc
@@ -21,11 +21,11 @@ void route_partition::from_hmetis_result(std::filesystem::path const& path, time
   auto const n_routes = tt.n_routes();
   route_to_cell_idx_.resize(n_routes, cell_idx_t::invalid());
 
-  this->n_cells_ = 0U;
   std::ifstream ifs(path, std::ifstream::in);
 
   std::string line;
   route_idx_t current_idx{0U};
+  cista::base_t<cell_idx_t> n_cells_{0U};
   while (std::getline(ifs, line)) {
     if (line.empty()) {
       break;
@@ -35,8 +35,18 @@ void route_partition::from_hmetis_result(std::filesystem::path const& path, time
     route_to_cell_idx_.at(current_idx) = assigned_cell;
     ++current_idx;
 
-    this->n_cells_ = std::max(this->n_cells_, static_cast<uint16_t>(assigned_cell.v_ + 1));
+    n_cells_ = std::max(n_cells_, static_cast<uint16_t>(assigned_cell.v_ + 1));
   }
+
+  if (n_cells_ == 0U) {
+    throw utl::fail("partition is empty!");
+  }
+
+  if (!std::has_single_bit(n_cells_)) {
+    throw utl::fail("we only support partitions with number of cells being a power of 2! Got {} cells", n_cells_);
+  }
+
+  this->n_levels_ = static_cast<cista::base_t<cell_idx_t>>(std::bit_width(n_cells_) - 1);
 }
 
 
